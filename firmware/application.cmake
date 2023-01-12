7b15904eaa18f9cd56549191ef968cedde48d2be
Dipun Mistry <dipunm@gmail.com>
2023-01-12 18:39:31 +0000

    optimised code

--- firmware/application.cmake
+++ firmware/application.cmake
@@ -1,16 +1,17 @@
 function (copy_dir TARGET SRC)
     file(GLOB_RECURSE SRC_FILES ${SRC}/*)
-
+    set(DST_FILES "")
     foreach(SRC_FILE ${SRC_FILES})
         get_filename_component(FILE_NAME ${SRC_FILE} NAME)
         set(DST_FILE ${CMAKE_CURRENT_BINARY_DIR}/../modules/${FILE_NAME})
-        add_custom_command(
-            OUTPUT ${DST_FILE}
-            COMMAND ${CMAKE_COMMAND} -E copy ${SRC_FILE} ${DST_FILE}
-            DEPENDS ${SRC_FILE}
-        )
-        target_sources(${TARGET} INTERFACE ${DST_FILE})
+        list(APPEND DST_FILES ${DST_FILE})
     endforeach()
+    add_custom_command(
+        OUTPUT ${DST_FILES}
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/../modules/
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${SRC} ${CMAKE_CURRENT_BINARY_DIR}/../modules/
+    )
+    target_sources(${TARGET} INTERFACE ${DST_FILES})
 endfunction()
 
 copy_dir(usermod_badger2040 ${PROJ_DIR}/src)

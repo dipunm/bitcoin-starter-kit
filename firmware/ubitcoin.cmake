b74798691f37dabefb499b15b284ae14db96327b
Dipun Mistry <dipunm@gmail.com>
2023-05-27 21:43:57 +0100

    try again

--- firmware/ubitcoin.cmake
+++ firmware/ubitcoin.cmake
@@ -3,15 +3,15 @@ add_library(ubitcoin INTERFACE)
 
 # Add our source files to the lib
 target_sources(ubitcoin INTERFACE
-    ${CMAKE_CURRENT_SOURCE_DIR}/uBitcoinWrapper.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/uBitcoinWrapper.cpp
+    ${FIRMWARE_DIR}/uBitcoinWrapper.c
+    ${FIRMWARE_DIR}/uBitcoinWrapper.cpp
     ${UBITCOIN_PATH}/src/HDWallet.cpp
     ${UBITCOIN_PATH}/src/Micropy.c
 )
 
 # Add the current directory as an include directory.
 target_include_directories(ubitcoin INTERFACE
-    ${CMAKE_CURRENT_SOURCE_DIR}
+    ${FIRMWARE_DIR}
     ${UBITCOIN_PATH}/src
 )
 

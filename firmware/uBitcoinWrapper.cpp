fdf0538c7ee34d9b8664c2dc526d444fc7a3de85
Dipun Mistry <dipunm@gmail.com>
2023-05-27 22:13:00 +0100

    try again

--- firmware/uBitcoinWrapper.cpp
+++ firmware/uBitcoinWrapper.cpp
@@ -6,8 +6,14 @@ extern "C" {
 
 mp_obj_t ubitcoin_fingerprint(void) {
 
-    HDPrivateKey hd(reinterpret_cast<const uint8_t*>("add good charge eagle walk culture book inherit fan nature seek repair"), "");
-    const char *message = hd.fingerprint()
+    HDPrivateKey hd(
+        reinterpret_cast<const uint8_t*>("add good charge eagle walk culture book inherit fan nature seek repair"), 
+        reinterpret_cast<const uint8_t*>("")
+    );
+
+    uint8_t fingerprintArray[4];
+    hd.fingerprint(fingerprintArray);
+    const char *message = reinterpret_cast<char*>(fingerprintArray);
     // const char *message = "Hello from uBitcoin!";
     return mp_obj_new_str(message, strlen(message));
 }
